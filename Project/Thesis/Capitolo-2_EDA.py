10d31a0b4c8d36ee6946f748492c2f4def84de88
Luigi-Tommasicchio <luigitomm0278@gmail.com>
2024-03-22 19:05:04 +0100

    commit

--- Project/Thesis/Capitolo-2_EDA.py
+++ Project/Thesis/Capitolo-2_EDA.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 # Download dei dati da Yahoo Finance
 ticker = 'AAPL'
-start_date = '2014-01-01'
+start_date = '2019-01-01'
 end_date = '2024-01-01'
 
 apple = yf.Ticker(ticker).history(ticker, start=start_date, end=end_date, interval='1d')[['Open', 'Close', 'High', 'Low', 'Volume']]
@@ -20,7 +20,17 @@ apple.isna().sum()
 
 # Riformatto l'indice da 'datetime' a 'date'
 apple = apple.reset_index()
-apple['Date'] = pd.to_datetime(apple['Date']).dt.date
+apple['Date'] = pd.to_datetime(apple['Date'].dt.date)
 apple.set_index('Date', inplace=True)
 apple.head()
 
+apple.info()
+
+# Plotto i dati come Candlestick Chart
+import mplfinance as mpf
+
+fig, ax = mpf.plot(apple, type='candle',volume=True, style='yahoo', 
+                ylabel='Prezzo', ylabel_lower='Volume', xlabel = 'Data', show_nontrading=False, 
+                figratio=(4,3), figscale=1.2, xrotation=45, returnfig=True)
+ax[0].set_title('Candlestick chart AAPL', fontsize=16, loc='center')
+plt.show()
