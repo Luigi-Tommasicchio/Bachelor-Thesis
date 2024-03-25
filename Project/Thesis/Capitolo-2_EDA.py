e97c1565e66e1cf00a91c147afc4d01440031c99
Luigi-Tommasicchio <luigitomm0278@gmail.com>
2024-03-25 14:28:54 +0100

    commit

--- Project/Thesis/Capitolo-2_EDA.py
+++ Project/Thesis/Capitolo-2_EDA.py
@@ -90,6 +90,7 @@ plt.show()
 
 # Creo tre benchmark per l'analisi:
 close_test = pd.DataFrame(close_test)
+
 # Benchmark mean
 mean_forecast = close_train.mean().repeat(252)
 close_test['mean_forecast'] = mean_forecast
@@ -102,13 +103,33 @@ close_test['naive_forecast'] = naive_forecast
 
 # Benchmark Drift
 drift = (close_train[-1] - close_train[0]) / len(close_train)  # Calcola il tasso di variazione medio
-
+drift
 drift_forecast = []
 for i in range(0, len(close_test)):
     forecast = close_train[-1] + (drift * i)
     drift_forecast.append(forecast)
 
 close_test['drift_forecast'] = drift_forecast
+
+
+# OLS
+import statsmodels.api as sm
+# Define your independent variable (X) and dependent variable (y)
+X = close_train.index.to_julian_date().values.reshape(-1, 1)  # Converting date index to Julian date
+y = close_train
+
+X = sm.add_constant(X)
+model = sm.OLS(y, X).fit()
+
+print(model.summary())
+
+X_test = close_test.index.to_julian_date().values.reshape(-1, 1) 
+X_test = sm.add_constant(X_test)
+
+ols_forecast = model.predict(X_test)
+close_test['ols_forecast'] = ols_forecast
+
+
 
 # Plotto i Benchmark
 import matplotlib.patheffects as pe
@@ -117,6 +138,7 @@ close_train.plot(label='Train set', color='blue', figsize=(15,7))
 close_test.Close.plot(label='Test set', color='red')
 close_test.naive_forecast.plot(label='Naive Forecast', color='green', lw=2)
 close_test.drift_forecast.plot(label='Drift Forecast', color='purple', lw=2)
+#close_test.ols_forecast.plot(label='OLS Forecast', color='black', lw=3)
 close_test.mean_forecast.plot(label='Mean Forecast', color='gold', lw=2, path_effects=[pe.Stroke(linewidth=2.5, foreground='black'), pe.Normal()])
 plt.ylabel('Prezzo $')
 plt.xlabel('Data')
@@ -124,6 +146,32 @@ plt.title('Prezzi di chiusura AAPL')
 plt.legend()
 plt.show()
 
+# Calcolo il varie misure di errote per le varie previsioni:
+from sklearn.metrics import mean_squared_error, root_mean_squared_error, mean_absolute_error, mean_absolute_percentage_error
+
+mean_mse = round(mean_squared_error(close_test.Close, close_test.mean_forecast),2)
+mean_rmse = round(root_mean_squared_error(close_test.Close, close_test.mean_forecast),2)
+mean_mae = round(mean_absolute_error(close_test.Close, close_test.mean_forecast),2)
+mean_mape = round(mean_absolute_percentage_error(close_test.Close, close_test.mean_forecast),2)
+
+naive_mse = round(mean_squared_error(close_test.Close, close_test.naive_forecast),2)
+naive_rmse = round(root_mean_squared_error(close_test.Close, close_test.naive_forecast),2)
+naive_mae = round(mean_absolute_error(close_test.Close, close_test.naive_forecast),2)
+naive_mape = round(mean_absolute_percentage_error(close_test.Close, close_test.naive_forecast),2)
+
+drift_mse = round(mean_squared_error(close_test.Close, close_test.drift_forecast),2)
+drift_rmse = round(root_mean_squared_error(close_test.Close, close_test.drift_forecast),2)
+drift_mae = round(mean_absolute_error(close_test.Close, close_test.drift_forecast),2)
+drift_mape = round(mean_absolute_percentage_error(close_test.Close, close_test.drift_forecast),2)
+
+benchmarks_errors = pd.DataFrame({
+    'MSE': [mean_mse, naive_mse, drift_mse],
+    'RMSE': [mean_rmse, naive_rmse, drift_rmse],
+    'MAE': [mean_mae, naive_mae, drift_mae],
+    'MAPE': [mean_mape, naive_mape, drift_mape]},
+    index = ['Mean Forecast', 'Naive Forecast', 'Drift Forecast'])
+
+benchmarks_errors
 
 
 
